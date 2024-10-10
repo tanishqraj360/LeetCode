7b621334b9dd836fb35d92bb0a3d2c7ad4ce2cc7
Tanishq Raj <tanishqraj306@gmail.com>
2024-10-10 12:55:04 +0530

    Minimum Number of Swaps to Make the String Balanced

---
+++ LeetCode1963.java
@@ -0,0 +1,17 @@
+class LeetCode1963 {
+  public int minSwaps(String s) {
+    int stackSize = 0;
+    for (int i = 0; i < s.length(); i++) {
+      char ch = s.charAt(i);
+
+      if (ch == '[')
+        stackSize++;
+      else {
+        if (stackSize > 0)
+          stackSize--;
+      }
+    }
+
+    return (stackSize + 1) / 2;
+  }
+}
