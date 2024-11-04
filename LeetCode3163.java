23385bab72f183c591c63806db14bb4635771796
Tanishq Raj <tanishqraj306@gmail.com>
2024-11-04 21:52:33 +0530

    String Compression III

---
+++ LeetCode3163.java
@@ -0,0 +1,22 @@
+public class LeetCode3163 {
+  public String compressedString(String word) {
+    StringBuilder comp = new StringBuilder("");
+
+    int pos = 0;
+
+    while (pos < word.length()) {
+      int consecutiveCount = 0;
+      char currentChar = word.charAt(pos);
+
+      while (pos < word.length() && consecutiveCount < 9 && word.charAt(pos) == currentChar) {
+        consecutiveCount++;
+        pos++;
+      }
+
+      comp.append(consecutiveCount).append(currentChar);
+
+    }
+    return comp.toString();
+
+  }
+}
