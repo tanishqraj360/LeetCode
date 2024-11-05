e8504d9623cdb8fe4c3759ce502c51a7d3d03a12
Tanishq Raj <tanishqraj306@gmail.com>
2024-11-05 23:31:53 +0530

    Set Matrix Zeroes

---
+++ LeetCode73.java
@@ -0,0 +1,32 @@
+public class LeetCode73 {
+  public void setZeroes(int[][] arr) {
+    int m = arr.length, n = arr[0].length;
+    boolean[] row = new boolean[m];
+    boolean[] col = new boolean[n];
+
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        if (arr[i][j] == 0) {
+          row[i] = true;
+          col[j] = true;
+        }
+      }
+    }
+
+    for (int i = 0; i < m; i++) {
+      if (row[i] == true) {
+        for (int j = 0; j < n; j++) {
+          arr[i][j] = 0;
+        }
+      }
+    }
+
+    for (int j = 0; j < n; j++) {
+      if (col[j] == true) {
+        for (int i = 0; i < m; i++) {
+          arr[i][j] = 0;
+        }
+      }
+    }
+  }
+}
