4557d7baaa4ecef7350aa470c222bea47b2404f7
Tanishq Raj <tanishqraj306@gmail.com>
2024-11-06 19:53:19 +0530

    Find if Array Can Be Sorted

---
+++ LeetCode3011.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class LeetCode3011 {
+  public boolean canSortArray(int[] nums) {
+    int[] values = Arrays.copyOf(nums, nums.length);
+
+    int n = values.length;
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n - i - 1; j++) {
+        if (values[j] <= values[j + 1]) {
+          continue;
+        } else {
+          if (Integer.bitCount(values[j]) == Integer.bitCount(values[j + 1])) {
+            int temp = values[j];
+            values[j] = values[j + 1];
+            values[j + 1] = temp;
+          } else {
+            return false;
+          }
+        }
+      }
+    }
+    return true;
+  }
+}
